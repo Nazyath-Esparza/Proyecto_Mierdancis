31a47600465a88757617f0792e99e0c41b2d74f0
Teresa Zaldivar <teresadelcarmen460@gmail.com>
2025-05-20 14:34:07 -0600

    Add files via upload

---
+++ ConsultaS5.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CONSULTA S 5</title>
+
+<style type="text/css">
+	
+table{background: white;
+	text-align: center;
+	border-collapse: collapse;
+	width: 70%;
+	color: black;
+	margin-top: 5%;
+	margin-left: 5%;
+
+}
+
+thead{
+	background: black;
+	border-bottom: solid 5px black ;
+	color: white;
+}
+
+th,td{padding: 1%;}
+
+tr:nth-child(even){background: #ddd; }
+
+tr:hover td{background: #9c3c27; 
+color: white;}
+
+
+</style>
+
+
+
+</head>
+<body>
+
+<?php
+
+$host="localhost";
+$username="root";
+$password="";
+$db_name="triple_baka_corporation";
+$tbl_name="discos";
+$conexion=mysqli_connect($host,$username,$password,$db_name)
+or die("Problemas con la conexion");
+
+$consulta= "SELECT * FROM discos WHERE precio<'3000.00'; ";
+$resultados= mysqli_query($conexion, $consulta);
+
+echo
+"<h1> MUESTRA TODAS LAS COLUMNAS Y
+<br>TODOS LOS CAMPOS DE LA TABLA DISCOS
+<br>DONDE EN SU PRECIO SEA MENOR A 3000.00 </h1>";
+echo "<br>";
+
+echo "<table>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>"."ID_Discos"."</th>";
+echo "<th>"."Nombre_album"."</th>";
+echo "<th>"."Id_compositores"."</th>";
+echo "<th>"."precio"."</th>";
+echo "<th>"."Id_Formato"."</th>";
+echo "</tr>";
+echo "</thead>";
+
+while ($fila = $resultados->fetch_assoc()) {
+	echo "<tr>;
+<td>". $fila["ID_Discos"]."</td>
+<td>". $fila['Nombre_album']."</td>
+<td>". $fila["Id_compositores"]."</td>
+<td>". $fila['precio']."</td>
+<td>". $fila["Id_Formato"]."</td>
+	</tr>";
+
+}
+
+echo "</table>";
+
+
+mysqli_close($conexion);
+
+?>
+
+</body>
+</html>
