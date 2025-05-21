52d7050c0ef233957db0a02e95a478a8883d0609
Makoto's bf <nazyath.esparza.chavez@cbtis89.edu.mx>
2025-05-21 13:49:15 -0600

    Add files via upload

---
+++ ConsultaE5.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CONSULTA E 5</title>
+
+<style type="text/css">
+	
+table{background: white;
+	text-align: center;
+	border-collapse: collapse;
+	width: 70%;
+	color: black;
+	margin-top: 5%;
+	margin-left: 5%;
+
+}
+
+thead{
+	background: black;
+	border-bottom: solid 5px black ;
+	color: white;
+}
+
+th,td{padding: 1%;}
+
+tr:nth-child(even){background: #ddd; }
+
+tr:hover td{background: #9c3c27; 
+color: white;}
+
+
+</style>
+
+
+
+</head>
+<body>
+
+<?php
+
+$host="localhost";
+$username="root";
+$password="";
+$db_name="triple_baka_corporation";
+$tbl_name="discos";
+$conexion=mysqli_connect($host,$username,$password,$db_name)
+or die("Problemas con la conexion");
+
+$consulta= "SELECT * FROM discos WHERE Id_Formato = '2';
+";
+$resultados= mysqli_query($conexion, $consulta);
+
+echo
+"<h1> Elimina todos los que tengan el formato de vinilo </h1>";
+echo "<br>";
+
+echo "<table>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>"."Id_Discos"."</th>";
+echo "<th>"."Nombre_album"."</th>";
+echo "<th>"."id_compositores"."</th>";
+echo "<th>"."precio"."</th>";
+echo "<th>"."id_formato"."</th>";
+echo "</tr>";
+echo "</thead>";
+
+while ($fila = $resultados->fetch_assoc()) {
+	echo "<tr>;
+<td>". $fila["ID_Discos"]."</td>
+<td>". $fila['Nombre_album']."</td>
+<td>". $fila["Id_compositores"]."</td>
+<td>". $fila['precio']."</td>
+<td>". $fila["Id_Formato"]."</td>
+	</tr>";
+
+}
+
+echo "</table>";
+
+mysqli_close($conexion);
+
+?>
+
+</body>
+</html>
